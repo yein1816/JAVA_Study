c36305bac85b0c0cb8350e96b418995b9482c9c5
Jay <beomgolaeda@gmail.com>
2025-01-09 19:21:54 +0900

    Add files via upload

---
+++ study1/s1.java
@@ -0,0 +1,12 @@
+package study1;
+
+public class s1 {
+    public static void main(String[] args) {
+     int num1 = 4;
+     int num2 = 3;
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+    }
+}
---
+++ study1/s3.java
@@ -0,0 +1,10 @@
+package study1;
+
+public class s3 {
+    public static void main(String[] args) {
+        long x = 10000000000L;
+        System.out.println(x);
+        boolean i = true;
+        System.out.println(i);
+    }
+}
